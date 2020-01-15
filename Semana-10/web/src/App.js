585078a5464e261a63891874e88893f12f97c07a
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-15 17:03:36 -0300

    Omnistack Semana 10: Criando formulário de cadastro e usuário

--- Semana-10/web/src/App.js
+++ Semana-10/web/src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
 
-// Componente ===================================================
-    /* O componente é alguma função que retorna conteúdo 
-    *  html. O App() abaixo é um componente. Ou seja, é um
-    *  bloco isolado de HTML, CSS e JS, o qual não interfere no 
-    *  restante da aplicação
-    */
-// Estado =======================================================
-    /*  É basicamente uma informação mantida pelo componente
-    *   (lembrar: imutabilidade)
-    */
-  
-// Propriedade ==================================================
-    /* Sempre que se usa HTML se usa propriedades, que são os atributos
-    *  tais como <Header title="Dashboard" />
-    *  São desta forma, informações que um componente "PAI" passa para o 
-    *  componente filho.
-    */
-
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
 
@@ -25,10 +11,85 @@ function App() {
   return (
     <div id="app">
       <aside>
-        
-      </aside>
-      <main>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+          <label htmlFor="github_username">Usuário do Github</label>
+          <input name="github_username" id="github_username" required />
+          </div>
 
+          <div class="input-block">
+          <label htmlFor="techs">Tecnologias</label>
+          <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+           <div class="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input name="latitude" id="latitude" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="longitude">Longitude</label>
+            <input name="longitude" id="longitude" required/>
+          </div>
+          </div>
+
+          <button type="submit">Salvar</button> 
+
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/28869405?s=460&v=4" alt="Thiago Costa" />
+              <div className="user-info">
+                <strong>Thiago Costa</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Informações sobre o perfil.</p>
+            <a href="https://github.com/thiagohrcosta">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/28869405?s=460&v=4" alt="Thiago Costa" />
+              <div className="user-info">
+                <strong>Thiago Costa</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Informações sobre o perfil.</p>
+            <a href="https://github.com/thiagohrcosta">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/28869405?s=460&v=4" alt="Thiago Costa" />
+              <div className="user-info">
+                <strong>Thiago Costa</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Informações sobre o perfil.</p>
+            <a href="https://github.com/thiagohrcosta">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/28869405?s=460&v=4" alt="Thiago Costa" />
+              <div className="user-info">
+                <strong>Thiago Costa</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Informações sobre o perfil.</p>
+            <a href="https://github.com/thiagohrcosta">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
 
     </div>
