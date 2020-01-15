a336ad1015a27057d2060c72698f4eef137911ba
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-15 16:21:20 -0300

    OmniStack Semana 10: Criando um counter no React

--- Semana-10/web/src/App.js
+++ Semana-10/web/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+// Componente
+    /* O componente é alguma função que retorna conteúdo 
+    *  html. O App() abaixo é um componente. Ou seja, é um
+    *  bloco isolado de HTML, CSS e JS, o qual não interfere no 
+    *  restante da aplicação
+    */
+// Estado
+    /*  É basicamente
+    */
+  
+// Propriedade
+    /* Sempre que se usa HTML se usa propriedades, que são os atributos
+    *  tais como <Header title="Dashboard" />
+    *  São desta forma, informações que um componente "PAI" passa para o 
+    *  componente filho.
+    */
+
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter(){
+    setCounter(counter + 1);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
   );
 }
 
