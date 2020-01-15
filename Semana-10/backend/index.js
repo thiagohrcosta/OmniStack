e755100bd3bc0a3b2d343187f4389c450dc72787
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-14 23:37:04 -0300

    OmniStack Semana 10: Entendendo conceitos sobre Query Params e Route Params

--- Semana-10/backend/index.js
+++ Semana-10/backend/index.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express()
 
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body:
+
 app.get('/', (request, response) => {
+    console.log(request.query);
     return response.json({ message: 'Hello OmniStack'});
 });
 
