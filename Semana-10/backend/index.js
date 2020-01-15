42204d58b65629b4596049aab984bfcb3fcbf427
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-15 00:02:30 -0300

    OmniStack Semana 10: Criando e iniciando MongoDB

--- Semana-10/backend/index.js
+++ Semana-10/backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = requere = require('mongoose');
 
-const app = express()
+const app = express();
+
+mongoose.connect('mongodb+srv://Thiago:<omnistack123456>@cluster0-z0kfu.mongodb.net/week10s?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
@@ -10,7 +16,9 @@ app.use(express.json());
 
 // Query Params: request.query (Filtros, ordenação, paginação, ...)
 // Route Params: request.params (Identificar um recurso na alteração ou remoção)
-// Body:
+// Body: request.body (Dados para criação ou alteraçaõ de um registro)
+
+// MongoDB (não-relacional)
 
 app.get('/', (request, response) => {
     console.log(request.query);
