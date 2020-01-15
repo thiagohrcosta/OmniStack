eb53628f1378fd447abbe32424cef29361d8afb6
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-15 00:21:27 -0300

    OmniStack Semana 10: Instalando AXIOS

--- Semana-10/backend/src/routes.js
+++ Semana-10/backend/src/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const axios = require('axios'); 
 
 const routes = Router();
 
 routes.get('/devs', (request, response) => {
     const { github_username }  = request.body;
+
+    const response = axios.get(`https://api.github.com/users/${github_username}`);
+
     return response.json({ message: 'Hello OmniStack'});
 });
 
