8d1fe4c4c8fe6093f6f1dc05b8e481d3f10873e4
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-15 18:38:19 -0300

    OmniStack Semana 10: Finalizando layout

--- Semana-10/backend/src/index.js
+++ Semana-10/backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = requere = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://Thiago:omnistack123456@cluster0-z0kfu.mongodb.ne
     useUnifiedTopology: true,
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
