9fcdf6616bc4c0c30a056d540ddae0bf816075f4
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-15 16:25:41 -0300

    OmniStack Semana 10: Limpando informações e iniciando projeto

--- Semana-10/web/src/App.js
+++ Semana-10/web/src/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-// Componente
+// Componente ===================================================
     /* O componente é alguma função que retorna conteúdo 
     *  html. O App() abaixo é um componente. Ou seja, é um
     *  bloco isolado de HTML, CSS e JS, o qual não interfere no 
     *  restante da aplicação
     */
-// Estado
-    /*  É basicamente
+// Estado =======================================================
+    /*  É basicamente uma informação mantida pelo componente
+    *   (lembrar: imutabilidade)
     */
   
-// Propriedade
+// Propriedade ==================================================
     /* Sempre que se usa HTML se usa propriedades, que são os atributos
     *  tais como <Header title="Dashboard" />
     *  São desta forma, informações que um componente "PAI" passa para o 
@@ -19,18 +20,18 @@ import React, { useState } from 'react';
 
 
 function App() {
-  const [counter, setCounter] = useState(0);
 
-  function incrementCounter(){
-    setCounter(counter + 1);
-
-  }
 
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        
+      </aside>
+      <main>
+
+      </main>
+
+    </div>
   );
 }
 
